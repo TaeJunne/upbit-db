cd50aa0a7df9139eb6a33b67e995cc84102cabfc
t-junne <99250658+TaeJunne@users.noreply.github.com>
2023-02-05 08:27:48 +0900

    0.1.0

---
+++ config/database.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const promise_1 = require("mysql2/promise");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const pool = (0, promise_1.createPool)({
+    port: parseInt(process.env.DB_PORT),
+    host: process.env.DB_HOSTNAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+exports.default = pool;
