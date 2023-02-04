b910903a2851740bb61d791839045803175c24ad
t-junne <99250658+TaeJunne@users.noreply.github.com>
2023-02-05 08:57:51 +0900

    코드 수정

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ import express from 'express'
 import { TokenController } from './token/token.controller'
 const app = express();
 
+const tokenController = new TokenController()
+tokenController.createMinutesCandle()
+
 app.listen(5000, () => {
   console.log(`✅ START CONNECTION 🚀 `)
 })
 
-const tokenController = new TokenController()
-
-tokenController.createMinutesCandle()
